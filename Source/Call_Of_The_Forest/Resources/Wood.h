6f990216b2ee53bc162a48c9dba0da01833d8461
apdRV <apodmarev@list.ru>
2024-02-23 18:46:36 +0300

    added test wood

---
+++ Source/Call_Of_The_Forest/Resources/Wood.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Wood.generated.h"
+#include <random>
+
+UCLASS()
+class CALL_OF_THE_FOREST_API AWood : public AActor
+{
+	GENERATED_BODY()
+	int Quantity;
+	
+public:	
+	// Sets default values for this actor's properties
+	AWood();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
