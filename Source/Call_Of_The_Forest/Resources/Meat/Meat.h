d75136d8a4f67fd742996a7bb5d85a3f87a110e0
apdRV <apodmarev@list.ru>
2024-02-24 17:51:42 +0300

    Added Meat, Base class, Stone, Wood and Base class of Trophys

---
+++ Source/Call_Of_The_Forest/Resources/Meat/Meat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Resources/ResourceBaseClass/ResourceBase.h"
+#include "Meat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALL_OF_THE_FOREST_API AMeat : public AResourceBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Call_Of_The_Forest/Resources/Trophy/TrophyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Resources/ResourceBaseClass/ResourceBase.h"
+#include "TrophyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALL_OF_THE_FOREST_API ATrophyBase : public AResourceBase
+{
+	GENERATED_BODY()
+	
+};
