fd6ce6d25a271e43c5b7313ba436c0e4e0d55c76
apdRV <apodmarev@list.ru>
2024-03-07 10:31:14 +0300

    Texture for wood

--- Source/Call_Of_The_Forest/Resources/Wood/Wood.cpp
+++ Source/Call_Of_The_Forest/Resources/Wood/Wood.cpp
@@ -1,5 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Wood.h"
+#include <iostream>
+
+AWood::AWood()
+{
+    Sprite = CreateDefaultSubobject<UPaperSpriteComponent>(FName("Wood"));
+
+    ConstructorHelpers::FObjectFinder<UPaperSprite> WoodSprite(TEXT("/Script/Paper2D.PaperFlipbook'/Game/Assets/log.log'"));
+
+    if (WoodSprite.Succeeded())
+    {
+        Sprite->SetSprite(WoodSprite.Object);
+        SetRootComponent(Sprite);
+    }
+}
+
+
+void AWood::BeginPlay()
+{
+    Super::BeginPlay();
+    SetActorScale3D(FVector(0.5, 1, 0.4));
+}
 
--- Source/Call_Of_The_Forest/Resources/Wood/Wood.h
+++ Source/Call_Of_The_Forest/Resources/Wood/Wood.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "../ResourceBaseClass/ResourceBase.h"
+#include "PaperSpriteComponent.h"
+#include "PaperSprite.h"
 #include "Wood.generated.h"
 
 /**
@@ -13,5 +15,13 @@ UCLASS()
 class CALL_OF_THE_FOREST_API AWood : public AResourceBase
 {
 	GENERATED_BODY()
+
+public:
+	AWood();
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Wood")
+    UPaperSpriteComponent* Sprite;
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
