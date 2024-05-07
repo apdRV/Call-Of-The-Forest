c1d28f77b80794f39091581de8811525682af724
apdRV <apodmarev@list.ru>
2024-05-07 14:09:22 +0300

    Some changes

--- Source/Call_Of_The_Forest/NPC/HobGoblin.cpp
+++ Source/Call_Of_The_Forest/NPC/HobGoblin.cpp
@@ -8,7 +8,8 @@ AHobGoblin::AHobGoblin()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	SetActorRotation(FRotator(0, 270, 90));
+	SetActorRelativeScale3D(FVector(0.6, 0.6, 0.6));
 	isInteracting = false;
 
 }
