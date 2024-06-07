b37560a6cc11f1d1027dc64f640248476fc7ec8f
mrnchk <143181833+mrnchk@users.noreply.github.com>
2024-06-07 18:39:06 +0300

    Update Predator.h

--- Source/Call_Of_The_Forest/Animals/Predator.h
+++ Source/Call_Of_The_Forest/Animals/Predator.h
@@ -3,161 +3,167 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "../World/AttackedDerivedDeclaration.h"
-#include "../World/StaticWorld.h"
-#include "Components/CapsuleComponent.h"
-#include "Engine/EngineTypes.h"
+#include "PaperCharacter.h"
+#include "PaperFlipbookActor.h"
+#include "PaperFlipbook.h"
 #include "Engine/World.h"
 #include "GameFramework/Character.h"
-#include "GameFramework/CharacterMovementComponent.h"
-#include "Net/UnrealNetwork.h"
-#include "PaperCharacter.h"
-#include "PaperFlipbook.h"
-#include "PaperFlipbookActor.h"
 #include "PaperFlipbookComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "../World/StaticWorld.h"
+#include "../World/AttackedDerivedDeclaration.h"
+#include "Engine/EngineTypes.h"
+#include "Net/UnrealNetwork.h"
 #include "Predator.generated.h"
 
 /**
- *
+ * 
  */
 UENUM(BlueprintType, Category = "Animation")
-enum class EPredatorState : uint8 {
-  LeftDown,
-  RightUp,
-  IdleLeftDown,
-  IdleRightUp,
-  AttackingLeftDown,
-  AttackingRightUp,
-  HitLeftDown,
-  HitRightUp,
-  DieLeftDown,
-  DieRightUp
+enum class EPredatorState : uint8{
+	LeftDown,
+	RightUp,
+	IdleLeftDown,
+	IdleRightUp,
+	AttackingLeftDown,
+	AttackingRightUp,
+	HitLeftDown,
+	HitRightUp,
+	DieLeftDown,
+	DieRightUp
 };
 UCLASS()
-class CALL_OF_THE_FOREST_API APredator : public APaperCharacter {
-  GENERATED_BODY()
+class CALL_OF_THE_FOREST_API APredator : public APaperCharacter
+{
+	GENERATED_BODY()
 
 public:
-  APredator();
+	APredator();
 
-  UFUNCTION()
-  virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+	virtual void Tick(float DeltaTime) override;
 
-  UFUNCTION()
-  virtual void BeginPlay() override;
+	UFUNCTION()
+	virtual void BeginPlay() override;
 
-  UFUNCTION()
-  float GetbDamage();
+	UFUNCTION()
+	float GetbDamage();
 
-  UFUNCTION()
-  bool GetbIsActive();
+	UFUNCTION()
+  	bool GetbIsActive();
 
-  UFUNCTION()
-  void SetbIsActive(bool Value);
+  	UFUNCTION()
+  	void SetbIsActive(bool Value);
 
-  UFUNCTION()
-  bool GetbIsTriggered();
+	UFUNCTION()
+	bool GetbIsTriggered();
 
-  UFUNCTION()
-  void SetbIsTriggered(bool Value);
+	UFUNCTION()
+	void SetbIsTriggered(bool Value);
 
-  UFUNCTION()
-  bool GetbIsDead();
+	UFUNCTION()
+	bool GetbIsDead();
 
-  UFUNCTION()
-  float GetbRadius();
+	UFUNCTION()
+	float GetbRadius();
 
-  UFUNCTION()
-  void SetbRadius(float Value);
+	UFUNCTION()
+	void SetbRadius(float Value);
 
-  UFUNCTION()
-  void SetbSpeed(float Value);
+	UFUNCTION()
+	void SetbSpeed(float Value);
 
-  UFUNCTION()
-  EPredatorState GetPredatorState();
+	UFUNCTION()
+	EPredatorState GetPredatorState();
 
-  UFUNCTION()
-  bool GetbIsDestroyed();
+	UFUNCTION()
+	bool GetbIsDestroyed();
 
-  UFUNCTION()
-  void SetbIsDestroyed(bool Value);
+	UFUNCTION()
+	void SetbIsDestroyed(bool Value);
 
-  UFUNCTION()
-  void SetPredatorState(EPredatorState NewState);
+	UFUNCTION()
+	void SetPredatorState(EPredatorState NewState);
 
-  UFUNCTION()
-  void SetPredatorLastState(EPredatorState NewState);
+	UFUNCTION()
+	void SetPredatorLastState(EPredatorState NewState);
+
 
 protected:
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
-  EPredatorState PredatorState;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	EPredatorState PredatorState;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
-  EPredatorState LastPredatorState;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	EPredatorState LastPredatorState;
 
-  UPROPERTY()
-  UPaperFlipbookComponent *PredatorFlipbookComponent;
+	UPROPERTY()
+	UPaperFlipbookComponent* PredatorFlipbookComponent;
 
-  UPROPERTY()
-  AStaticWorld *World;
+	UPROPERTY()
+	AStaticWorld* World;
 
-  UPROPERTY()
-  float bSpeed;
+	UPROPERTY()
+	float bSpeed;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
-  float bHealth;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	float bHealth;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
-  float bDamage;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	float bDamage;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
-  bool bIsDead;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	bool bIsDead;
 
-  UPROPERTY()
-  bool bIsAttacking;
+	UPROPERTY(Replicated)
+	bool bIsAttacking;
 
-  UPROPERTY()
-  bool bIsDestroyed;
+	UPROPERTY()
+	bool bIsDestroyed;
 
-  UPROPERTY()
-  FTimerHandle AttackTimerHandle;
+	UPROPERTY()
+	FTimerHandle AttackTimerHandle;
 
-  UPROPERTY()
-  bool bIsTriggered;
+	UPROPERTY()
+	bool bIsTriggered;
 
-  UPROPERTY()
-  bool bIsActive;
+	UPROPERTY()
+	bool bIsActive;
 
-  UPROPERTY()
-  float bRadius;
+	UPROPERTY()
+	float bRadius;
 
-  UFUNCTION()
-  void Attacked(float Value);
+	UFUNCTION()
+	void Attacked(float Value);
 
-  UFUNCTION()
-  void SetAttackAnimation();
+	UFUNCTION()
+	void SetAttack();
 
-  UFUNCTION()
-  void EndAttackAnimation();
+	UFUNCTION()
+	void SetAttackAnimation();
 
-  UFUNCTION()
-  virtual void UpdatePredatorSprite();
+	UFUNCTION()
+	void EndAttackAnimation();
 
-  UFUNCTION()
-  void UpdatePredatorState();
+	UFUNCTION()
+	virtual void UpdatePredatorSprite();
 
-  UFUNCTION()
-  void SetupOwner(UPaperFlipbookComponent *m_owner);
+	UFUNCTION()
+	void UpdatePredatorState();
 
-  UFUNCTION()
-  virtual void Die();
+	UFUNCTION()
+	void SetupOwner(UPaperFlipbookComponent* m_owner);
+	
+	UFUNCTION()
+	virtual void Die();
 
-  // for server
-  virtual void GetLifetimeReplicatedProps(
-      TArray<FLifetimeProperty> &OutLifetimeProps) const override;
+	//for server 	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 private:
-  // Access to protected members for attacking system
-  friend class AttackedActor;
-  friend class AttackingActor;
+	//Access to protected members for attacking system
+	friend class AttackedActor;
+	friend class AttackingActor;
+
+
 };
